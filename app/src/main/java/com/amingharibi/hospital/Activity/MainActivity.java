63bf5abee0f14269da3dc05792cfca856ed2b113
M-AminGharibi <126321624+M-AminGharibi@users.noreply.github.com>
2024-07-18 04:51:18 +0330

    Optimize Code

--- app/src/main/java/com/amingharibi/hospital/Activity/MainActivity.java
+++ app/src/main/java/com/amingharibi/hospital/Activity/MainActivity.java
@@ -3,15 +3,8 @@ package com.amingharibi.hospital.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.amingharibi.hospital.R;
 import com.amingharibi.hospital.databinding.ActivityMainBinding;
 
 
